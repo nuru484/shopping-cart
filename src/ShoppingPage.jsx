ee6989f481016755b76da25aeaf220a77372ec75
Nurudeen Abdul-Majeede <abdulmajeednurudeen48@gmail.com>
2024-05-03 00:09:12 -0800

    Added the parts of the shoping page ie nav, main and footer

--- src/ShoppingPage.jsx
+++ src/ShoppingPage.jsx
@@ -3,9 +3,35 @@ import { Link } from 'react-router-dom';
 const ShoppingPage = () => {
   return (
     <div>
-      <Link to={'/'}>Home</Link>
-      <Link to={'/shopping-page'}>Shop</Link>
-      <div>Welcome to the shopping page</div>
+      <nav>
+        <ul>
+          <li>Logo</li>
+          <li>
+            <input type="search" />
+          </li>
+          <li>
+            <Link to={'/'}>Home</Link>
+          </li>
+          <li>
+            <Link to={'/shopping-page'}>Shop</Link>
+          </li>
+          <li>Cart</li>
+        </ul>
+      </nav>
+      <main>
+        <p>Item1</p>
+        <p>Item2</p>
+        <p>Item3</p>
+      </main>
+      <footer>
+        <div>
+          <p>Company Name</p>
+          <p>&copy; 2024 Abdul-Majeed Nurudeen. All rights reserved.</p>
+        </div>
+        <div>
+          <a href="https://github.com/nuru484">Source Code</a>
+        </div>
+      </footer>
     </div>
   );
 };
